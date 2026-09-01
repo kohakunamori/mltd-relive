146211527273241194a9df35e3b45b05bb24d683
Kohaku Namori <2633047525@qq.com>
2026-09-01 23:37:46 +0900

    Trim SQLite query instrumentation overhead

--- standalone/mltd/models/engine.py
+++ standalone/mltd/models/engine.py
@@ -1,3 +1,4 @@
+import logging
 import time
 
 from sqlalchemy import create_engine, event
@@ -18,20 +19,28 @@ def set_sqlite_pragma(dbapi_connection, connection_record):
     cursor.execute('PRAGMA busy_timeout=5000')
     cursor.execute('PRAGMA journal_mode=WAL')
     cursor.execute('PRAGMA synchronous=NORMAL')
+    cursor.execute('PRAGMA temp_store=MEMORY')
+    cursor.execute('PRAGMA cache_size=-32768')
     cursor.close()
 
 
 @event.listens_for(Engine, 'before_cursor_execute')
 def before_cursor_execute(conn, cursor, statement,
                           parameters, context, executemany):
+    if not logger.isEnabledFor(logging.DEBUG):
+        return
     conn.info.setdefault('query_start_time', []).append(time.perf_counter_ns())
     logger.debug(f'Start query: {statement}')
     logger.debug(f'Parameters: {parameters}')
 
 
-@event.listens_for(Engine, "after_cursor_execute")
+@event.listens_for(Engine, 'after_cursor_execute')
 def after_cursor_execute(conn, cursor, statement,
                          parameters, context, executemany):
-    total_ns = time.perf_counter_ns() - conn.info['query_start_time'].pop(-1)
-    logger.debug('Query complete!')
-    logger.debug(f'Query execution time: {total_ns // 1_000_000} ms')
+    if not logger.isEnabledFor(logging.DEBUG):
+        return
+    starts = conn.info.get('query_start_time')
+    if not starts:
+        return
+    total_ns = time.perf_counter_ns() - starts.pop(-1)
+    logger.debug(f'Query complete in {total_ns / 1_000_000:.2f} ms')
